05658bab52d0eef6c137c80097f323138ca69faa
DrTCortes <thiagocortes@outlook.com.br>
2022-09-28 18:58:07 -0300

    style(web): adiciona a biblioteca Phosphor icons ao projeto e altera o font family para Inter

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,9 @@
+import { MagnifyingGlassPlus } from 'phosphor-react'
+
 import './styles/main.css';
+
 import logoImg from './assets/logo-nlw-esports.svg'
+
 function App() {
 
   return (
@@ -75,8 +79,8 @@ function App() {
             <span className="text-zinc-400 font-normal block">Publique um anúncio para encontrar novos players!</span>
           </div>
 
-          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 rounded">
-            <img src="" alt="" />
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24} color="white"/>
             <span className="text-white">Publicar anúncio</span>
           </button>
         </div>
